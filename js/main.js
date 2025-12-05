11c84d35c7bb42f0b671f0c7ef62bde155363246
Eadric Ng Eadric Ng <eehowng6@gmail.com>
2025-12-05 15:45:44 +0800

    mainpage

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // 跳转到登录页面
 function goToLogin() {
-    window.location.href = 'login.html';
+    window.location.href = 'login.php';
 }
 
 // 跳转到注册页面
 function goToRegister() {
-    window.location.href = 'register.html';
+    window.location.href = 'register.php';
 }
 
 // 登出函数
